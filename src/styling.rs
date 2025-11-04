002ea6a7cced56b0b8405b237ee64ad7894cdba9
John O'Farrell <johnofarrell12@gmail.com>
2025-11-04 02:36:58 -0500

    mono theme tweaks
    adding graymono and updating softmono

--- src/styling.rs
+++ src/styling.rs
@@ -22,6 +22,7 @@ impl AppStyling {
             "lightmono" => Ok(Self::lightmono_theme()),
             "redmono" => Ok(Self::redmono_theme()),
             "softmono" => Ok(Self::softmono_theme()),
+            "graymono" => Ok(Self::graymono_theme()),
             "nord" => Ok(Self::nord_theme()),
             "nord-bg" => Ok(Self::nord_bg_theme()),
             "gruvbox" => Ok(Self::gruvbox_theme()),
@@ -29,7 +30,7 @@ impl AppStyling {
             "goldgreen-light" => Ok(Self::goldgreen_light_theme()),
             "goldgreen-dark" => Ok(Self::goldgreen_dark_theme()),
             _ => Err(format!(
-                "Invalid theme '{}'. Valid themes: monochrome, lightmono, redmono, softmono, nord, nord-bg, gruvbox, rosepine, goldgreen-light, goldgreen-dark",
+                "Invalid theme '{}'. Valid themes: monochrome, lightmono, redmono, softmono, graymono, nord, nord-bg, gruvbox, rosepine, goldgreen-light, goldgreen-dark",
                 theme
             )),
         }
@@ -137,7 +138,7 @@ impl AppStyling {
     }
 
     fn softmono_theme() -> Self {
-        const SOFT_WHITE: &str = "#FCF6F8"; 
+        const SOFT_WHITE: &str = "#FCF6F8";
 
         Self {
             border_style: Style::default().fg(Color::Black).bg(Self::hex_color(SOFT_WHITE)),
@@ -145,10 +146,26 @@ impl AppStyling {
             text_style: Style::default().fg(Color::Black).bg(Self::hex_color(SOFT_WHITE)),
             selected_text_style: Style::default()
                 .fg(Color::Black)
-                .bg(Color::Black),  // Black on black = solid black boxes
+                .bg(Color::Black),  // Black on black = solid black boxes (previously visited + toggled current)
+            current_selected_style: Style::default().fg(Color::Black),  // Black text, no background (default state)
+            density_bar_style: Style::default().fg(Color::Black).bg(Self::hex_color(SOFT_WHITE)),  // Same as border
+            border_type: BorderType::Plain,
+            use_background_fill: true,  // Enable background fill for seamless soft white background
+        }
+    }
+
+    fn graymono_theme() -> Self {
+        const SOFT_WHITE: &str = "#FCF6F8";
+        const SOFT_GRAY: &str = "#8B8B8B";
+
+        Self {
+            border_style: Style::default().fg(Color::Black).bg(Self::hex_color(SOFT_WHITE)),
+            highlighted_border_style: Style::default().fg(Color::Black).bg(Self::hex_color(SOFT_WHITE)),
+            text_style: Style::default().fg(Color::Black).bg(Self::hex_color(SOFT_WHITE)),
+            selected_text_style: Self::hex_style(SOFT_GRAY),  // Soft gray text, no background (previously visited + toggled current)
             current_selected_style: Style::default()
                 .fg(Color::Black)
-                .bg(Color::Black),  // Black on black = solid black boxes
+                .bg(Color::Black),  // Black on black = darker highlight (default state)
             density_bar_style: Style::default().fg(Color::Black).bg(Self::hex_color(SOFT_WHITE)),  // Same as border
             border_type: BorderType::Plain,
             use_background_fill: true,  // Enable background fill for seamless soft white background
